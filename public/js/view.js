9da17b6d847d366bd148795e0f31a719fad85fce
negasimichael <negasimichael1@gmail.com>
2021-02-13 13:04:19 -0600

    first commit

---
+++ public/js/view.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+
+  const form = document.getElementById('todo-form')
+  const newTodoInput = document.querySelector('input.new-item')
+  const todoListSpan = document.querySelector('.todo-container')
+
+  const getTodos = () => {
+    fetch('/api/todos')
+      .then(response => response.json())
+      .then(todos => renderTodoList(todos))
+  }
+
+  const renderTodoList = todos => {
+    const todosHTML = todos.map(todo => {
+      return `<li class="list-group-item todo-item">
+        <span>${todo.text}</span>
+        <input type="text" class="edit" style="display: none;">
+        <button data-id="${todo.id}" class="delete btn btn-danger">x</button>
+        <button data-id="${todo.id}" class="complete btn btn-primary">✓</button>
+      </li>`
+    }).join('')
+    todoListSpan.innerHTML = todosHTML
+  }
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    const text = newTodoInput.value
+    fetch('/api/todos', {
+      method: 'POST',
+      body: JSON.stringify({ text }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(getTodos)
+      .catch(err => console.error(err))
+  })
+
+  getTodos()
+});
---
+++ routes/api-routes.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
+
+router.get('/api/todos',async(req,res) =>{
+try{
+  const todos = await db.Todo.findAll()
+    res.json(todos)
+} catch(err){
+    res.status(500).send(err)
+}
+})
+
+router.post('/api/todos', async (req, res) => {
+  try {
+    await db.Todo.create(req.body)
+    res.status(200).send()
+  } catch(err) {
+    res.status(500).send(err)
+  }
+})
+
+// router.put()
+
+// router.delete()
+
+module.exports = router
